0af4bf05a1f3c84eadd959f67326cc341bb70d66
bassem-krayem <bassem.krayem09@gmail.com>
2024-09-23 02:57:57 +0100

    task1

---
+++ TypeScript/task_1/js/main.js
@@ -0,0 +1,8 @@
+var teacher3 = {
+    firstName: "John",
+    lastName: "Doe",
+    fullTimeEmployee: false,
+    location: "London",
+    contract: false, // Dynamic attribute
+};
+console.log(teacher3);
