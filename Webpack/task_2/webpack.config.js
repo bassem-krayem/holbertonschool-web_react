7761a21a4eaf7496eed251c2dd95860c9bbd094c
bassem-krayem <bassem.krayem09@gmail.com>
2026-01-19 12:03:50 +0100

    feat: task2 from the project webpack

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public/"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/i,
+        use: ["file-loader", "image-webpack-loader"],
+        type: "asset/resource",
+      },
+    ],
+  },
+};
